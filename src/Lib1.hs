000775abadd2e63968fbc306333928be73958d22
Mantas <mantas.ziln@gmail.com>
2023-09-26 20:47:11 +0300

    padariau pirmą ir antrą, ir pradėjau trečią. Nesu tikras ar antras gerai padarytas, bet testus praeina, o su trečiu nelabai žinau kaip toliau

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -17,20 +17,45 @@ type Database = [(TableName, DataFrame)]
 
 -- Your code modifications go below this comment
 
+toLowerCase :: Char -> Char
+toLowerCase c
+ | 'A' <= c && c <= 'Z' = toEnum (fromEnum c + 32)
+ | otherwise = c
+
+comparer :: a -> [b] -> Boll 
+comparer :: a
+
 -- 1) implement the function which returns a data frame by its name
 -- in provided Database list
 findTableByName :: Database -> String -> Maybe DataFrame
-findTableByName _ _ = error "findTableByName not implemented"
+findTableByName [] _ = Nothing
+findTableByName _ [] = Nothing
+findTableByName (x:xs) name 
+  | fst x == dataFrameName = Just (snd x)
+  | otherwise = findTableByName xs dataFrameName
+    where 
+      dataFrameName = map toLowerCase name
+
+
 
 -- 2) implement the function which parses a "select * from ..."
 -- sql statement and extracts a table name from the statement
 parseSelectAllStatement :: String -> Either ErrorMessage TableName
-parseSelectAllStatement _ = error "parseSelectAllStatement not implemented"
+parseSelectAllStatement "" = Left "Error - empty input"
+parseSelectAllStatement input
+  | length input' < 15 = Left "Error - invalid input"
+  | map toLowerCase (take 14 input') /= "select * from " = Left "Error - invalid input"
+  | otherwise = Right (drop 14 input')
+    where
+      input' = case last input of
+        ';' -> take (length input - 1) input
+        _ -> input
 
 -- 3) implement the function which validates tables: checks if
 -- columns match value types, if rows sizes match columns,..
 validateDataFrame :: DataFrame -> Either ErrorMessage ()
-validateDataFrame _ = error "validateDataFrame ot implemented"
+validateDataFrame (DataFrame columns rows)
+  | 
 
 -- 4) implement the function which renders a given data frame
 -- as ascii-art table (use your imagination, there is no "correct"
