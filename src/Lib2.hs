3b9764fd842c6876852228cc59861d4c2cd47ebe
Vytautas Mielkus <mielkus.vytautas@gmail.com>
2023-10-25 00:29:14 +0300

    Implemented WHERE with AND's and tested, everything seems to work

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -14,7 +14,7 @@ import Lib1 (renderDataFrameAsTable)
 import qualified Text.Parsec as P
 import Text.Parsec ((<?>))
 import Data.Char (toUpper, toLower)
-import Data.List (isPrefixOf, elemIndex, find)
+import Data.List (isPrefixOf, isInfixOf, elemIndex, find)
 import Data.Maybe (isJust, mapMaybe, catMaybes, fromJust)
 
 type ErrorMessage = String
@@ -28,11 +28,17 @@ data ParsedStatement
     | SelectMin { columns :: [String], fromTable :: TableName }
     | SelectWithMin { minColumns :: [String], otherColumns :: [String], fromTable :: TableName }
     | SelectAvg { columns :: [String], fromTable :: TableName }
+    | SelectWithConditions { selectedColumns :: [String], fromTable :: TableName, conditions :: [Condition] }
     | StatementWithFilters { columns :: [String], database :: String, filters :: [String] }
     | StatementWithoutFilters { columns :: [String], database :: String }
     | StatementSelectAll { database :: String }
     deriving (Show, Eq)
-    
+ 
+--data Condition = EqualsCondition String Value  deriving (Show, Eq)
+data Condition = EqualsCondition String ConditionValue  deriving (Show, Eq)
+
+--data Value = IntegerValue Int | StringValue String deriving (Show, Eq)
+data ConditionValue = IntegerConditionValue Int | StringConditionValue String deriving (Show, Eq)
     
 --------------------------------------------------- Parser ----------------------------------------------------
 
@@ -42,26 +48,27 @@ data ParsedStatement
 -- - Wildcard selection (e.g., SELECT * FROM table)
 -- - Column list, MIN function with/without columns, AVG function
 
-columnNameParser :: P.Parsec String () String
-columnNameParser = P.many1 (P.alphaNum P.<|> P.char '_')
-columnsListParser :: P.Parsec String () [String]
-columnsListParser = columnNameParser `P.sepBy1` (P.char ',' >> P.many P.space)
-
 -- task 3, column list
     
 selectParser :: P.Parsec String () ParsedStatement
 selectParser = do
-    _ <- caseInsensitiveString "SELECT" <?> "SELECT keyword"
+    _ <- caseInsensitiveString "SELECT"
     _ <- P.many P.space
-    selectType <- (P.try (P.char '*' >> return (StatementSelectAll "")) <?> "Wildcard '*'")
-               P.<|> ((columnsListParser >>= \cols -> return (SelectFrom cols "")) <?> "Column list")
-    _ <- P.many P.space
-    _ <- caseInsensitiveString "FROM" <?> "FROM keyword"
-    _ <- P.many P.space
-    tablename <- P.many1 (P.alphaNum P.<|> P.char '_') <?> "Table name"
-    case selectType of
-        StatementSelectAll _ -> return $ StatementSelectAll tablename
-        SelectFrom cols _   -> return $ SelectFrom cols tablename
+    selectType <- P.try (do
+                        _ <- P.char '*'
+                        _ <- P.many P.space
+                        _ <- caseInsensitiveString "FROM"
+                        _ <- P.many P.space
+                        tablename <- P.many1 (P.alphaNum P.<|> P.char '_')
+                        return $ StatementSelectAll tablename)
+                  P.<|> (do
+                        cols <- columnsListParser
+                        _ <- P.many P.space
+                        _ <- caseInsensitiveString "FROM"
+                        _ <- P.many P.space
+                        tablename <- P.many1 (P.alphaNum P.<|> P.char '_')
+                        return $ SelectFrom cols tablename)
+    return selectType
         
 --task 3, MIN function
         
@@ -110,6 +117,31 @@ avgParser = do
     _ <- P.many P.space
     tablename <- P.many1 (P.alphaNum P.<|> P.char '_')
     return $ SelectAvg columns tablename
+    
+--task 3, WHERE with AND, e.g.: SELECT id, surname FROM employees WHERE id=1 AND name="Vi" (AND is not required, and it still works)
+
+conditionParser :: P.Parsec String () Condition
+conditionParser = do
+    column <- columnNameParser
+    _ <- P.many P.space
+    _ <- P.char '='
+    _ <- P.many P.space
+    value <- valueParser
+    return $ EqualsCondition column value
+    
+valueParser :: P.Parsec String () ConditionValue
+valueParser = P.try (P.many1 P.digit >>= \digits -> return $ IntegerConditionValue (read digits))
+         P.<|> (P.char '"' >> P.manyTill P.anyChar (P.char '"') >>= \str -> return $ StringConditionValue str)
+         
+selectWithWhereParser :: P.Parsec String () ParsedStatement
+selectWithWhereParser = do
+    selectStmt <- selectParser  -- Use the select parser you already have
+    _ <- P.many P.space
+    _ <- caseInsensitiveString "WHERE"
+    _ <- P.many P.space
+    conditions <- conditionParser `P.sepBy1` (P.many P.space >> caseInsensitiveString "AND" >> P.many P.space)
+    return $ SelectWithConditions (selectedColumns selectStmt) (fromTable selectStmt) conditions
+
 
 -- Parses user input into an entity representing a parsed statement
 parseStatement :: String -> Either ErrorMessage ParsedStatement
@@ -122,13 +154,16 @@ parseStatement input
             case P.parse (P.try minWithOtherColumnsParser P.<|> minParser) "" input of
                 Left err -> Left $ "Parse Error: " ++ show err
                 Right stmt -> Right stmt
+    | "WHERE" `isInfixOf` (map toUpper input) = 
+            case P.parse selectWithWhereParser "" input of
+                Left err -> Left $ "Parse Error: " ++ show err
+                Right stmt -> Right stmt
     | "SELECT" `isPrefixOf` (map toUpper input) = 
         case P.parse selectParser "" input of
             Left err -> Left $ "Parse Error: " ++ show err
             Right stmt -> Right stmt
     | otherwise = Left "Invalid SQL command"
-
-        
+      
         
 --------------------------------------------------- Execute ----------------------------------------------------    
     
@@ -218,6 +253,31 @@ executeStatement (SelectAvg columns tableName) db =
             in Right $ DataFrame (map (\c -> Column c FloatType) columns) [avgValues]
         Nothing -> Left "Table not found"
         
+--execute for WHERE with AND
+
+executeStatement (SelectWithConditions selectedCols tablename conditions) db = 
+    case lookup tablename db of
+        Just df -> do
+            let dfWithConditionsApplied = applyConditions conditions df
+            let DataFrame allColumns allRows = dfWithConditionsApplied
+
+            -- If selectedCols is "*", select all columns
+            let finalSelectedCols = if selectedCols == ["*"] 
+                                    then map (\(Column name _) -> name) allColumns 
+                                    else selectedCols
+            
+            -- Extract only the columns specified in the SELECT statement
+            let validIndices = mapMaybe (\col -> 
+                                         let colType = columnTypeByName col allColumns in
+                                         case colType of 
+                                             Just t -> elemIndex (Column col t) allColumns
+                                             Nothing -> Nothing) finalSelectedCols
+            let newRows = map (\row -> map (row !!) validIndices) allRows
+            let newCols = map (allColumns !!) validIndices
+            
+            Right $ DataFrame newCols newRows
+        Nothing -> Left "Table not found"
+        
 --execute for SELECT * FROM tablename
 
 executeStatement (StatementSelectAll tableName) db = 
@@ -236,71 +296,81 @@ executeStatement _ _ = Left "Statement not supported or invalid"
 -- iterateUntil "grabber hands" 'n'
 -- >> "grabber ha"
 -- note that it doesn't return the specified element on the end
-iterateUntil :: String -> Char -> [Char]
-iterateUntil [] _ = []
-iterateUntil (x:xs) a
-  |x /= a = x : iterateUntil xs a
-  |x == a = []
+
+--iterateUntil :: String -> Char -> [Char]
+--iterateUntil [] _ = []
+--iterateUntil (x:xs) a
+--  |x /= a = x : iterateUntil xs a
+--  |x == a = []
 
 -- splits a string into words by using a specific element
 -- egz.:
 -- splitString "the quick brown     fox jumped     over the lazy dog" ' ' 
 -- >> ["the","quick","brown","fox","jumped","over","the","lazy","dog"]
-splitString :: String -> Char -> [String]
-splitString [] _ = []
-splitString statement a
- | head statement == a = (splitString . tail $ statement) a
- | otherwise = (iterateUntil statement a) : splitString (drop ( (1 + ) . length $ iterateUntil statement a)  statement) a
+
+--splitString :: String -> Char -> [String]
+--splitString [] _ = []
+--splitString statement a
+-- | head statement == a = (splitString . tail $ statement) a
+-- | otherwise = (iterateUntil statement a) : splitString (drop ( (1 + ) . length $ iterateUntil statement a)  statement) a
 
 -- separate a certain element from from string
 -- egz.:
 -- separateElement ",my,spoon,,is too,big," ','
 -- >> ["," "my" "," "spoon" "," "," "is too" "," "big" ","]
-separateElement :: String -> Char -> [String]
-separateElement [] _ = []
-separateElement (x:xs) a 
- | x == a = (x : []) : (separateElement xs a)
- | elem a (x:xs) = (iterateUntil (x:xs) a) : (a : []) : separateElement (drop ( (1 + ) . length $ iterateUntil (x:xs) a)  (x:xs) ) a
- | otherwise = (x:xs) : []
+
+--separateElement :: String -> Char -> [String]
+--separateElement [] _ = []
+--separateElement (x:xs) a 
+-- | x == a = (x : []) : (separateElement xs a)
+-- | elem a (x:xs) = (iterateUntil (x:xs) a) : (a : []) : separateElement (drop ( (1 + ) . length $ iterateUntil (x:xs) a)  (x:xs) ) a
+-- | otherwise = (x:xs) : []
 
 -- splits a query into words and separates commas
 -- egz.: 
 -- splitToComponents "grass, grass,brass,,sash"
 -- >> ["grass" "," "grass" "," "brass" "," "," "sash"]
-splitToComponents :: String -> [String]
-splitToComponents [] = []
-splitToComponents query = 
-  let 
-    words = splitString query ' '
-    y = head words
-    ys = tail words
 
-    temp :: [String] -> [String]
-    temp [] = []
-    temp (x:[]) = separateElement x ','
-    temp (x:xs) = separateElement x ',' ++ temp xs
-  in 
-    separateElement y ',' ++ temp ys
+--splitToComponents :: String -> [String]
+--splitToComponents [] = []
+--splitToComponents query = 
+--  let 
+--    words = splitString query ' '
+--    y = head words
+--    ys = tail words
+--
+--    temp :: [String] -> [String]
+--    temp [] = []
+--    temp (x:[]) = separateElement x ','
+--    temp (x:xs) = separateElement x ',' ++ temp xs
+--  in 
+--    separateElement y ',' ++ temp ys
 
 -- separateElement but only separates ';' in the last word
 -- separateLastSemicolon ["select","*","from","data;"]
 -- >> ["select","*","from","data",";"]
-separateLastSemicolon :: [String] -> [String]
-separateLastSemicolon [] = []
-separateLastSemicolon (x : []) = separateElement x ';' ++ [] 
-separateLastSemicolon (x : xs) = x : separateLastSemicolon xs
 
-toLowerCase :: Char -> Char
-toLowerCase c
- | 'A' <= c && c <= 'Z' = toEnum (fromEnum c + 32)
- | otherwise = c
+--separateLastSemicolon :: [String] -> [String]
+--separateLastSemicolon [] = []
+--separateLastSemicolon (x : []) = separateElement x ';' ++ [] 
+--separateLastSemicolon (x : xs) = x : separateLastSemicolon xs
+--
+--toLowerCase :: Char -> Char
+--toLowerCase c
+-- | 'A' <= c && c <= 'Z' = toEnum (fromEnum c + 32)
+-- | otherwise = c
  
 columnName :: Column -> String
 columnName (Column name _) = name
 
--- Extracts the type from a Column data structure
+-- extracts the type from a Column data structure
 columnType :: Column -> ColumnType
 columnType (Column _ t) = t
+
+columnNameParser :: P.Parsec String () String
+columnNameParser = P.many1 (P.alphaNum P.<|> P.char '_')
+columnsListParser :: P.Parsec String () [String]
+columnsListParser = columnNameParser `P.sepBy1` (P.char ',' >> P.many P.space)
 
 elemAt :: Int -> [a] -> Maybe a
 elemAt idx xs
@@ -310,7 +380,32 @@ elemAt idx xs
 columnTypeByName :: String -> [Column] -> Maybe ColumnType
 columnTypeByName colName cols = columnType <$> find (\(Column name _) -> name == colName) cols
 
+-- helper functions for WHERE with AND
+
+applyConditions :: [Condition] -> DataFrame -> DataFrame
+applyConditions conditions (DataFrame cols rows) = DataFrame cols (filter (meetAllConditions cols conditions) rows)
+
+meetAllConditions :: [Column] -> [Condition] -> Row -> Bool
+meetAllConditions columns conditions row = all (\cond -> meetCondition columns cond row) conditions
+
+meetCondition :: [Column] -> Condition -> Row -> Bool
+meetCondition columns (EqualsCondition colName condValue) row = 
+    case elemIndex colName (map columnName columns) of
+        Just idx -> (row !! idx) == convertConditionValueToValue condValue
+        Nothing -> False
+  where
+    convertValue :: Value -> ConditionValue
+    convertValue (IntegerValue int) = IntegerConditionValue (fromIntegral int)
+    convertValue (StringValue str) = StringConditionValue str
+    
+    convertConditionValueToValue :: ConditionValue -> Value
+    convertConditionValueToValue (IntegerConditionValue int) = IntegerValue (fromIntegral int)
+    convertConditionValueToValue (StringConditionValue str) = StringValue str
+        
+-- helper function for case insensitiveness for all sql keywords: select, from, min, avg, where, etc.
+
 caseInsensitiveString :: String -> P.Parsec String () String
 caseInsensitiveString s = P.try (mapM caseInsensitiveChar s)
     where
         caseInsensitiveChar c = P.char (toLower c) P.<|> P.char (toUpper c)
+        
